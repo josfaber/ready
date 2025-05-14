e954eb3a12db4fec9649ff8836de5ff7013d1122
Jos <josfaber@me.com>
2025-05-14 15:22:20 +0200

    Filament install, admin user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Beheerder',
+            'email' => 'dev+admin@soapbox.nl',
+            'password' => Hash::make('dev+admin@soapbox.nl'),
+        ]);
+    }
+}
