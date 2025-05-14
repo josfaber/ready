ecd938ad5ceeae85dce66c77c4a83fda8ca253fc
Jos <josfaber@me.com>
2025-05-14 19:22:58 +0200

    Refactor UserSeeder to correct admin user details and update user creation logic

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Book;
+use App\Models\User;
 use App\Models\Genre;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -14,7 +15,11 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
+        // All users, but the admin which has id === 1
+        $userIds = array_diff(User::pluck('id')->toArray(), [1]);
+
         Book::factory(100)->create([
+            'user_id' => fn () => fake()->randomElement($userIds),
         ])->each(function ($book) {
             $book->genres()->attach(Genre::inRandomOrder()->take(rand(1, 3))->pluck('id'));
         });
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,15 +14,22 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Beheerder',
+            'id' => 1,
+            'name' => 'Admin',
             'email' => 'dev+admin@soapbox.nl',
             'password' => Hash::make('dev+admin@soapbox.nl'),
         ]);
 
         User::factory()->create([
-            'name' => 'Jos',
-            'email' => 'dev+jos@soapbox.nl',
-            'password' => Hash::make('dev+jos@soapbox.nl'),
+            'name' => 'Kaj',
+            'email' => 'dev+kaj@soapbox.nl',
+            'password' => Hash::make('dev+kaj@soapbox.nl'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Mads',
+            'email' => 'dev+mads@soapbox.nl',
+            'password' => Hash::make('dev+mads@soapbox.nl'),
         ]);
     }
 }
