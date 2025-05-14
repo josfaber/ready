1d310e9fd5c4da6176a60d2c3ec5c62c2ec18882
Jos <josfaber@me.com>
2025-05-14 19:24:10 +0200

    Add enums.php for book status translations

---
+++ resources/lang/en/enums.php
@@ -0,0 +1,9 @@
+<?php
+// filepath: /Users/jos/workspace/ready/resources/lang/en/enums.php
+return [
+    'book-status' => [
+        'has-read' => 'Has Read',
+        'reading' => 'Reading',
+        'want-to-read' => 'Want to Read',
+    ],
+];
